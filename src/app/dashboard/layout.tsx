47dcf4e6eecd09573533065c73d93baa58d76dd7
Mikelitom <majfe202@hotmail.com>
2025-03-25 15:48:52 -0700

    Colegiatura page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,20 +1,22 @@
 'use client'
 import type React from 'react';
-import { useState } from 'react';
 import { ReactNode } from 'react';
 import Sidebar from '../components/sidebar/Sidebar';
 import Navbar from '../components/navbar/Navbar';
+import { DashboardProvider } from '@/contexts/DashboardContext';
 
-export default function DashboardLayout(this: any, { children }: { children: ReactNode }) {
+export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex h-screen">
-      <Sidebar />
-      <div className="flex flex-col flex-1">
-        <Navbar title='Inicio' user='Miguel Fajardo'/>
-        <main className="p-6">
-          {children}
-        </main>
+    <DashboardProvider>
+      <div className="flex h-screen">
+        <Sidebar />
+        <div className="flex flex-col flex-1">
+          <Navbar />
+          <main className="p-6">
+            {children}
+          </main>
+        </div>
       </div>
-    </div>
+    </DashboardProvider>
   );
 }
